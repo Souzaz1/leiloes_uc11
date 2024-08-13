cfd7ff49609cf5357808ad1af887eae3721f0f80
Souzaz1 <gabrielpsouza89@gmail.com>
2024-08-13 17:53:48 -0300

    listagem de produtos funcionando.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -52,7 +52,24 @@ public class ProdutosDAO {
     }
 
     public ArrayList<ProdutosDTO> listarProdutos() {
+        conn = new conectaDAO().connectDB();
+        String sql = "SELECT * FROM produtos";
 
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                listagem.add(produto);
+            }
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + erro.getMessage());
+        }
         return listagem;
     }
 
